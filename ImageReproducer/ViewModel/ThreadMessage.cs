f5822cd453864ee392663705476217a235943914
AntoineAugay <a.augay@laposte.net>
2017-10-12 15:39:56 +0200

    Add basic statistic
    Add :
    - total elapse time
    - generation mean time
    - best score

--- ImageReproducer/ViewModel/ThreadMessage.cs
+++ ImageReproducer/ViewModel/ThreadMessage.cs
@@ -6,11 +6,14 @@ namespace ImageReproducer.ViewModel
     {
         public Mat Image { get; set; }
         public int GenerationNumber { get; set; }
+        public int BestFitness { get; set; }
 
-        public ThreadMessage(Mat Image, int GenerationNumber)
+
+        public ThreadMessage(Mat Image, int GenerationNumber, int BestFitness)
         {
             this.Image = Image;
             this.GenerationNumber = GenerationNumber;
+            this.BestFitness = BestFitness;
         }
     }
 }
